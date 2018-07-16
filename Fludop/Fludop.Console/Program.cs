899ec0df85d6721b4b2ff1997405b4c04c662291
Wojtek_Lap <wkx100@gmail.com>
2018-07-16 20:53:26 +0200

    Add basic operations SELECT, UPDATE, INSERT

--- Fludop/Fludop.Console/Program.cs
+++ Fludop/Fludop.Console/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using Fludop.Core;
 
 namespace Fludop.Console
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            var testFludop = TestFludop();
+            System.Console.WriteLine(testFludop);
+            System.Console.ReadKey();
+        }
+
+        private static string TestFludop()
+        {
+            var fludop = new Core.Fludop();
+
+            var result = fludop
+                .Select("Column1", "Column2", "Column3")
+                .From("Table1")
+                .Build();
+
+            var selectAll = fludop
+                .Select()
+                .From("Table1")
+                .Build();
+
+            var insert = fludop
+                .Insert("Table1", "Value1", "Value2", "Value3")
+                .Build();
+
+            var update = fludop
+                .Update("Books")
+                .Set("Title", "New Title")
+                .Where("Number", "100")
+                .Build();
+
+            return result;
         }
     }
 }
--- Fludop/Fludop/Core/Fludop.cs
+++ Fludop/Fludop/Core/Fludop.cs
@@ -6,5 +6,88 @@ namespace Fludop.Core
 {
     public class Fludop
     {
+        private readonly StringBuilder _stringBuilder;
+
+        public Fludop()
+        {
+            _stringBuilder = new StringBuilder();
+        }
+
+        public Fludop Select(params string[] columns)
+        {
+            _stringBuilder.Append("SELECT ");
+
+            if (columns.Length > 0)
+            {
+                foreach (var column in columns)
+                {
+                    _stringBuilder.Append($"{column}, ");
+                }
+                _stringBuilder.Remove(_stringBuilder.Length - 2, 1);
+            }
+            else
+            {
+                _stringBuilder.Append("* ");
+            }
+
+            return this;
+        }
+
+        public Fludop From(string table)
+        {
+            _stringBuilder.Append($"FROM {table}");
+            return this;
+        }
+
+        public Fludop Update(string entity)
+        {
+            string updateQuery = $"UPDATE {entity} ";
+            _stringBuilder.Append(updateQuery);
+            return this;
+        }
+
+        public Fludop Set(string column, string value)
+        {
+            string setQuery = $"SET {column}='{value}' ";
+            _stringBuilder.Append(setQuery);
+            return this;
+        }
+
+        public Fludop Where(string column, string value)
+        {
+            string whereQuery = $"WHERE {column}='{value}'";
+            _stringBuilder.Append(whereQuery);
+            return this;
+        }
+
+        public Fludop Insert(string table, params string[] values)
+        {
+            string insertQuery = $"INSERT INTO {table} VALUES (";
+            _stringBuilder.Append(insertQuery);
+
+            foreach (var value in values)
+            {
+                _stringBuilder.Append($"{value}, ");
+            }
+
+            _stringBuilder.Remove(_stringBuilder.Length - 2, 2);
+            _stringBuilder.Append(")");
+
+            return this;
+        }
+
+        public string Build()
+        {
+            _stringBuilder.Append(";");
+            var query = _stringBuilder.ToString();
+            _stringBuilder.Clear();
+            return query;
+        }
+
+        private void EnsureCommandCreated()
+        {
+            if (_stringBuilder == null)
+                throw new InvalidOperationException();
+        }
     }
 }
